4d3fb8675f5254d533783076cb63703dfd045be1
joshuafolkken <joshuafolkken@gmail.com>
2025-07-21 16:48:08 +0900

    background #41

---
+++ scripts/background.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+@export var scroll_speed := 50.0
+
+@onready var sprite: Sprite2D = $ParallaxLayer/Sprite2D
+
+
+func _process(delta: float) -> void:
+	sprite.region_rect.position += delta * Vector2(scroll_speed, scroll_speed)
+
+	if sprite.region_rect.position >= Vector2(64.0, 64.0):
+		sprite.region_rect.position = Vector2.ZERO
