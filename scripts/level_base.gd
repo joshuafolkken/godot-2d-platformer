a61372dfcaeb262bff9822ff4d0df564a4073d82
joshuafolkken <joshuafolkken@gmail.com>
2025-07-22 16:07:53 +0900

    customize #43

--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -4,3 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().paused = false
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed(ActionName.RESET):
+		GameManager.load_main_scene()
