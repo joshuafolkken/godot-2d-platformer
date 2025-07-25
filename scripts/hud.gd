fabe1a80b353a5d046f7a44939c9684c390278ed
Joshua Folkken <joshuafolkken@gmail.com>
2025-07-25 14:33:16 +0900

    stages #54

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -25,5 +25,5 @@ func _on_game_complete() -> void:
 
 func _process(_delta: float) -> void:
 	if background.visible:
-		if Input.is_action_just_pressed(ActionName.JUMP):
-			GameManager.load_main_scene()
+		if Input.is_action_just_pressed(ActionName.F):
+			GameManager.load_next_stage()
--- scripts/singletons/game_manager.gd
+++ scripts/singletons/game_manager.gd
@@ -3,6 +3,15 @@ extends Node
 const MAIN_SCENE: PackedScene = preload("res://scenes/main.tscn")
 const LEVEL_SCENE: PackedScene = preload("res://scenes/level_base.tscn")
 
+const STAGES: Array[String] = [
+	"walk",
+	"jump",
+	"2-jumps",
+	"dash-jump",
+]
+
+var _current_stage := -1
+
 
 func _change_scene(packed_scene: PackedScene) -> void:
 	get_tree().change_scene_to_packed(packed_scene)
@@ -10,7 +19,18 @@ func _change_scene(packed_scene: PackedScene) -> void:
 
 func load_main_scene() -> void:
 	_change_scene(MAIN_SCENE)
+	_current_stage = 0
 
 
-func load_level_scene() -> void:
-	_change_scene(LEVEL_SCENE)
+func load_stage(number: int) -> void:
+	if number < 0 or number >= STAGES.size():
+		load_main_scene()
+		return
+
+	var stage_scene: PackedScene = load("res://scenes/stage-%s.tscn" % STAGES[number])
+	_change_scene(stage_scene)
+
+
+func load_next_stage() -> void:
+	_current_stage += 1
+	load_stage(_current_stage)
