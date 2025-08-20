4f1c6ffb9041b88057ccd40f16e8e6e5cf6fe513
joshuafolkken <joshuafolkken@gmail.com>
2025-08-21 05:47:05 +0900

    virtual gamepad #55

--- scenes/ui/virtual_gamepad.gd
+++ scenes/ui/virtual_gamepad.gd
@@ -1,16 +1,16 @@
 extends Control
 
-# ボタンとアクション名のマッピング
-var _button_actions: Array[String] = [
-	ActionName.LEFT,
-	ActionName.RIGHT,
-	ActionName.UP,
-	ActionName.DOWN,
-	ActionName.JUMP,
-	ActionName.DASH,
-	ActionName.RESET,
-	ActionName.INTERACT
+const MOBILE_KEYWORDS: Array[String] = [
+	"Android", "iPhone", "iPad", "iPod", "BlackBerry", "Windows Phone"
 ]
+const ARROW_ACTIONS: Array[String] = [
+	ActionName.LEFT, ActionName.RIGHT, ActionName.UP, ActionName.DOWN
+]
+const BUTTON_ACTIONS: Array[String] = [
+	ActionName.JUMP, ActionName.DASH, ActionName.RESET, ActionName.INTERACT
+]
+
+var _actions := ARROW_ACTIONS + BUTTON_ACTIONS
 
 
 func get_button_name(action_name: String) -> String:
@@ -21,20 +21,44 @@ func get_action_name(button_name: String) -> String:
 	return button_name.to_lower()
 
 
+func _is_mobile_web_browser() -> bool:
+	var navigator: JavaScriptObject = JavaScriptBridge.get_interface("navigator")
+	@warning_ignore("unsafe_property_access")
+	var user_agent: String = navigator.userAgent
+
+	for keyword: String in MOBILE_KEYWORDS:
+		if keyword in user_agent:
+			return true
+
+	return false
+
+
 func _is_touch_device() -> bool:
 	if DisplayServer.is_touchscreen_available():
 		return true
 
 	if OS.has_feature("web"):
-		var navigator: JavaScriptObject = JavaScriptBridge.get_interface("navigator")
-		@warning_ignore("unsafe_property_access")
-		var user_agent: String = navigator.userAgent
-		var mobile_keywords := ["Android", "iPhone", "iPad", "iPod", "BlackBerry", "Windows Phone"]
-		for keyword: String in mobile_keywords:
-			if keyword in user_agent:
-				return true
+		return _is_mobile_web_browser()
 
 	return false
+
+
+func _get_parent_node_name(action_name: String) -> String:
+	return "Arrows" if action_name in ARROW_ACTIONS else "Buttons"
+
+
+func _setup_button(action_name: String) -> void:
+	var button_name := get_button_name(action_name)
+	var parent_node_name := _get_parent_node_name(action_name)
+	var node_path := "%s/%s" % [parent_node_name, button_name]
+
+	var touch_screen_button: TouchScreenButton = get_node_or_null(node_path)
+
+	if not touch_screen_button:
+		return
+
+	touch_screen_button.pressed.connect(_on_pressed.bind(button_name))
+	touch_screen_button.released.connect(_on_released.bind(button_name))
 
 
 func _ready() -> void:
@@ -44,44 +68,22 @@ func _ready() -> void:
 		hide()
 		return
 
-	for action_name: String in _button_actions:
-		var button_name := get_button_name(action_name)
-		var parent_node_name := ""
-		var touch_screen_button: TouchScreenButton
-
-		if action_name in [ActionName.LEFT, ActionName.RIGHT, ActionName.UP, ActionName.DOWN]:
-			parent_node_name = "Arrows"
-		else:
-			parent_node_name = "Buttons"
-
-		var node_path := "%s/%s" % [parent_node_name, button_name]
-		touch_screen_button = get_node_or_null(node_path)
-
-		if touch_screen_button:
-			touch_screen_button.pressed.connect(_on_touch_screen_button_pressed.bind(button_name))
-			touch_screen_button.released.connect(_on_touch_screen_button_released.bind(button_name))
+	for action_name: String in _actions:
+		_setup_button(action_name)
 
 
-func _parse_input_event(action_name: String, pressed := true) -> void:
-	var input_event := InputEventAction.new()
-	input_event.action = action_name
-	input_event.pressed = pressed
-	Input.parse_input_event(input_event)
-
-
-func _on_touch_screen_button_pressed(button_name: String) -> void:
+func _on_pressed(button_name: String) -> void:
 	var action_name := get_action_name(button_name)
 	if action_name:
 		Input.action_press(action_name)
-		AudioManager.play_sfx(AudioManager.SoundId.SYSTEM)
 
 
-func _on_touch_screen_button_released(button_name: String) -> void:
+func _on_released(button_name: String) -> void:
 	var action_name := get_action_name(button_name)
 	if action_name:
 		Input.action_release(action_name)
 
 
 func _exit_tree() -> void:
-	for action_name: String in _button_actions:
+	for action_name: String in _actions:
 		Input.action_release(action_name)
